8e89934db55d0ba8df91c10ef6ea2bd2cfee10bf
Chakria Lim <lmc5000a@ensfsi.onmicrosoft.com>
2024-10-22 18:57:42 +0200

    creation de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,62 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonPreferee;
+	private int argent;
+	
+	public void parler(String texte) {
+		System.out.println("(" + getNom() + ") - " + texte );
+	}
+	
+
+	public Humain(String nom, String boissonPreferee, int argent) {
+		this.nom = nom;
+		this.boissonPreferee = boissonPreferee;
+		this.argent = argent;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public int getArgent() {
+		return argent;
+	}
+	
+	public String getBoissonPreferee() {
+		return boissonPreferee;
+	}
+	
+	public void direBonjour() {
+		String texte;
+		texte = "Bonjour ! Je m'appelle " + getNom() + "et j'aime boire du";
+		parler(texte);
+	}
+	
+	public void boire() {
+		String texte;
+		texte = "Mmmm, un bon verre de " + getBoissonPreferee() + " ! GLOUPS!";
+		parler(texte);
+	}
+	
+	public void acheter(String bien, int prix) {
+		String texte;
+		if (argent>=prix) {
+			texte = "J'ai " + getArgent() + "sous en poche. Je vais m'offrir un " + bien + " à " + prix + " sous";
+			perdreArgent(prix);
+		} else {
+			texte = "Je n'ai plus que " + getArgent() + "sous en poche. Je ne peux même pas m'offrir un " + bien + "à " + prix + "sous."; 
+		}
+		parler(texte);
+	}
+	
+	public void gagnerArgent(int gain) {
+		argent += gain;
+	}
+	
+	public void perdreArgent(int perte) {
+		argent -= perte;
+	}
+}
